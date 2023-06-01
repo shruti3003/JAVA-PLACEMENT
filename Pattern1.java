907be84a144975da258d2442d084b67afa3ceb5f
Shruti Vijay Tibile <84867329+shruti3003@users.noreply.github.com>
2023-06-01 09:04:27 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        System.out.print("****\n***\n**\n*\n");
+
+        int a = 2;
+        int b = 10;
+        System.out.println(a);
+        System.out.println(b);
+
+        a = b;
+
+        System.out.println(a);
+
+        String name = "shruti tibile";
+        System.out.println(name);
+
+    }
+
+}
