324ddea5b3723a189ab74eb4f7f11bcc63a143e4
Shruti Vijay Tibile <84867329+shruti3003@users.noreply.github.com>
2023-06-01 08:32:35 +0530

    Add files via upload

---
+++ BASIC.java
@@ -0,0 +1,6 @@
+public class BASIC {
+    public static void main(String[] args) {
+        System.out.println("hi shruti how are you");
+        System.out.print("hello\nhi\nheyyy\nwhatsupppp");
+    }
+}
